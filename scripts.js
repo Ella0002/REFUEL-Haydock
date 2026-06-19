fa59630d76c92c17bb8becce513cc5b94973f0e2
Ella0002 <ella.heyes247@gmail.com>
2026-06-19 13:23:36 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("backToTop");
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            btn.classList.add("show");
+        } else {
+            btn.classList.remove("show");
+        }
+    });
+
+    btn.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
